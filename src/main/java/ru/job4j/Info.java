a8be021cc15e216f53a58da6359cf021c3b665a2
Nikola <NAA9m902m>
2022-04-24 17:35:12 +0700

    Дата

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class Info {
-    public static void main(String [] args) {
-        System.out.println ("22.04.2022");
+    public static void main(String[] args) {
+        System.out.println("22.04.2022");
     }
 }
