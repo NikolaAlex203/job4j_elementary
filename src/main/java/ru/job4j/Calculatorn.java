2ce62b7e434f17c1d6b6ade663ae9625f0b466ea
Nikola <NAA9m902m>
2022-04-28 00:42:08 +0700

    Фит

---
+++ src/main/java/ru/job4j/Calculatorn.java
@@ -0,0 +1,15 @@
+package ru.job4j.calculatorn;
+
+public class Calculatorn {
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+    public static void main(String[] args) {
+        Calculatorn.plus(1, 2);
+        Calculatorn.plus(10, 11);
+        Calculatorn.plus(100, 500);
+        Calculatorn.plus(4, 2);
+        Calculatorn.plus(3, 5);
+    }
+}
