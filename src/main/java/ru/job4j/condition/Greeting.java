fe0b8a67e48715cfb0d50aaf6af250cfadaaf855
Nikola <NAA9m902m>
2022-05-04 21:16:46 +0700

    9.1. String [#237407]

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -4,7 +4,7 @@ public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
         System.out.println(idea);
-        idea = "But I am a newbie.";
+        idea = idea + "But I am a newbie.";
         int year = 2022;
         idea = idea + 2022;
         System.out.println(idea);
