f193aae55857ee328fad6d56ccd283f5a75db474
Nikola <NAA9m902m>
2022-06-01 19:28:50 +0700

    4.4. Вложенный оператор if else. Запутанный код [#190 #307070]

--- src/main/java/ru/job4j/condition/DivideBySix.java
+++ src/main/java/ru/job4j/condition/DivideBySix.java
@@ -16,6 +16,6 @@ public class DivideBySix {
     }
 
     public static void main(String[] args) {
-        System.out.println(checkNumber(14));
+        System.out.println(checkNumber(29));
     }
 }
