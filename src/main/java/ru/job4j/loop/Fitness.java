91ddfef35f99d471e9357cd0343fcb6d5c752217
Nikola <NAA9m902m>
2022-06-26 15:06:39 +0700

    5.4. Протеиновая диета [#156313]

---
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan <= nik) {
+            ivan *= 3;
+            nik *= 2;
+            month += 1;
+        }
+        return month;
+    }
+
+    public static void main(String[] args) {
+        int m = Fitness.calc(95, 90);
+        System.out.println("Иван обгонит Николая через " + m + " месяц/месяца/месяцев");
+    }
+}
+
